dce7f407bef4ebc044383157c8ff56362fc28554
Layel <glezrdg@gmail.com>
2023-03-19 17:56:51 -0400

    2%

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Grade } from 'src/app/interfaces/grade';
 import { Student } from 'src/app/interfaces/student';
 import { GradeService } from 'src/app/services/grade.service';
 import { StudentService } from 'src/app/services/student.service';
@@ -11,19 +13,60 @@ import { StudentService } from 'src/app/services/student.service';
 })
 export class StudentComponent implements OnInit {
   id: number;
+  courseId: number = 0;
+  students: Student[] = [];
+  filteredStudents: Student[] = [];
+  form: FormGroup;
+  operation = 'Add';
+  studentId!: number;
+  grade: Grade = { gradeId: 0, name: '', students: [] };
+  inputSearch = '';
+
   constructor(
     private _studentService: StudentService,
     private _gradeService: GradeService,
-    private aRoute: ActivatedRoute
+    private aRoute: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
   ) {
     this.id = +this.aRoute.snapshot.paramMap.get('id')!;
     console.log(this.id);
+
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+    });
   }
-  courseId: number = 0;
-  students: Student[] = [];
+
+  @ViewChild('modal') modal!: ElementRef;
+
+  searchValueChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.inputSearch = value;
+    console.log(this.inputSearch);
+    this.filteredStudents = this.students.filter((p) =>
+      p.name.toLowerCase().includes(this.inputSearch.toLocaleLowerCase())
+    );
+  }
+
+  choosedStudents() {
+    if (this.filteredStudents.length > 0) {
+      return this.filteredStudents;
+    } else return this.students;
+  }
 
   ngOnInit(): void {
     this.obtainStudentByCourse();
+    this.obtainOneGrade(this.id);
+  }
+  ngOnChanges() {}
+
+  openModal() {
+    this.modal.nativeElement.showModal();
+    console.log(this.modal);
+  }
+  closeModal() {
+    this.modal.nativeElement.close();
+    console.log(this.modal);
   }
 
   obtainStudents() {
@@ -33,7 +76,14 @@ export class StudentComponent implements OnInit {
   }
 
   obtainOneStudent(id: number) {
-    this._studentService.getOneStudent(id).subscribe((data) => {});
+    this._studentService.getOneStudent(id).subscribe((data) => {
+      this.operation = 'Edit';
+      this.studentId = data.id!;
+      this.openModal();
+      this.form.setValue({
+        name: data.name,
+      });
+    });
   }
   obtainStudentByCourse() {
     this._studentService.getStudentByCourse(this.id).subscribe((data) => {
@@ -47,10 +97,51 @@ export class StudentComponent implements OnInit {
       console.log('obtained');
     });
   }
+  obtainOneGrade(id: number) {
+    this._gradeService.getOneGrade(id).subscribe((data) => {
+      this.grade = data;
+    });
+  }
+  addStudent(student: Student) {
+    this._studentService.createStudent(student).subscribe((data) => {
+      console.log(data);
+      this.obtainStudentByCourse();
+      this.closeModal();
+    });
+  }
+
+  addOrEditStudent() {
+    const student: Student = {
+      name: this.form.value.name,
+      gradeId: this.id,
+    };
+    if (this.operation == 'Edit') {
+      this._studentService
+        .updateStudent(this.studentId, student)
+        .subscribe((data) => {
+          console.log(data);
+          this.obtainStudentByCourse();
+          this.closeModal();
+        });
+    } else {
+      this.addStudent(student);
+    }
+  }
 
   removeGrade(id: number) {
     this._gradeService.deleteGrade(id).subscribe((data) => {
       this.obtainGrade();
+      this.router.navigate(['/']);
     });
   }
+  removeStudent(id: number) {
+    this._studentService.deleteStudent(id).subscribe((data) => {
+      this.obtainStudentByCourse();
+    });
+  }
+  // updateOneStudent() {
+  //   this._studentService.updateStudent().subscribe((data) => {
+
+  //   })
+  // }
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -9,17 +9,31 @@ import { Student } from '../interfaces/student';
 })
 export class StudentService {
   private myAppUrl: string = enviroment.endpoint;
-  private myApiUrl: string = 'api/Student/Course/';
+  private myApiUrl: string = 'api/Student/';
 
   constructor(private http: HttpClient) {}
 
   getStudents() {
     return this.http.get<Student[]>(`${this.myAppUrl}${this.myApiUrl}`);
   }
-  getOneStudent(id: Number) {
-    return this.http.get(`${this.myAppUrl}${this.myApiUrl}${id}`);
+  getOneStudent(id: Number): Observable<Student> {
+    return this.http.get<Student>(`${this.myAppUrl}${this.myApiUrl}${id}`);
   }
   getStudentByCourse(id: number) {
-    return this.http.get<Student[]>(`${this.myAppUrl}${this.myApiUrl}${id}`);
+    return this.http.get<Student[]>(
+      `${this.myAppUrl}${this.myApiUrl}Course/${id}`
+    );
+  }
+  createStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(`${this.myAppUrl}${this.myApiUrl}`, student);
+  }
+  deleteStudent(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.myAppUrl}${this.myApiUrl}${id}`);
+  }
+  updateStudent(id: number, student: Student): Observable<void> {
+    return this.http.put<void>(
+      `${this.myAppUrl}${this.myApiUrl}${id}`,
+      student
+    );
   }
 }
